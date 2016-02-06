c6bd4e8eb9fa0bf7665dcb0c553b6069271d928f
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-06 11:29:59 +0200

    methods without binding

--- src/Component.js
+++ src/Component.js
@@ -8,13 +8,25 @@ class SimpleComponent extends Component {
         }
     }
 
+    componentDidMount() {
+
+    }
+
     render() {
         return (
             <div>
                 Counter: {this.state.counter}
+                <a href = "#" onClick = {this.handleClick}>increment</a>
             </div>
         )
     }
+
+    handleClick = (ev) => {
+        ev.preventDefault()
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    };
 }
 
 export default SimpleComponent
